be4c366a3021353dcae21c79470fec493b0629df
Ludwig LEDE <ludwig.engelhardt@gmail.com>
2024-07-23 17:15:02 +0200

    app setup

--- sokudoku/src/App.tsx
+++ sokudoku/src/App.tsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Start from "./views/start";
+import NotFound from "./views/notFound";
+import Setting from "./views/setting";
+import "./index.css";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
