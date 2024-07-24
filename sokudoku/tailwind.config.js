468b66470f0bc1e60fb93e1b558c0eef3077d3ef
Ludwig LEDE <ludwig.engelhardt@gmail.com>
2024-07-24 09:48:31 +0200

    setup for color palates in tailwind

--- sokudoku/tailwind.config.js
+++ sokudoku/tailwind.config.js
@@ -5,7 +5,60 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        palatte1: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte2: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte3: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte4: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte5: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte6: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte7: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte8: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte9: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+        palatte10: {
+          primary: '#ff6347',
+          secondary: '#4caf50',
+          accent: '#1e90ff',
+        },
+      },
+    },
   },
   plugins: [],
 };
