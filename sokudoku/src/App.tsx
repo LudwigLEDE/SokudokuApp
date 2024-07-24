6f014e4a90df07fdf7a883fd2931b1e60ccc689b
Ludwig LEDE <ludwig.engelhardt@gmail.com>
2024-07-24 09:43:09 +0200

    updated main and app

--- sokudoku/src/App.tsx
+++ sokudoku/src/App.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Start from "./views/start";
 import NotFound from "./views/notFound";
 import Setting from "./views/setting";
-import "./index.css";
+import SudokuGame from "./views/game";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/start" element={<Start />} />
         <Route path="/setting" element={<Setting />} />
+        <Route path="/game/:difficulty" element={<SudokuGame />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
